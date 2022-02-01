24b4c1b77dea45243610fba55df6220756020584
Alejandro Marrero Cabrera <44745574+alexey1254@users.noreply.github.com>
2022-02-01 19:35:10 +0000

    Add files via upload

---
+++ instalarApps.sh
@@ -0,0 +1,29 @@
+## Instalador de apps de preferencia mia para vms
+## Ejecutar como admin
+
+echo "--------------------- Se va a instalar MariaDB ---------------------"
+
+sudo apt update
+sudo apt install mariadb-server -y
+
+echo "--------------------- Se ha instalado mariaDB ---------------------"
+
+sudo add-apt-repository ppa:fkrull/deadsnakes
+sudo apt-get update
+sudo apt-get install python3.9 -y
+
+echo "--------------------- Se ha instalado python 3.9 ---------------------"
+
+sudo apt install openjdk-17-jdk -y
+
+echo "--------------------- Se ha instalado Java 17 ---------------------"
+
+sudo sudo snap install code --classic
+
+echo "--------------------- Se ha instalado VS-Code ---------------------"
+
+echo " ---------------------Ahora se va a configurar mysql ---------------------"
+
+sudo mysql_secure_installation
+
+echo "--------------------- Se han completado todas las tareas ---------------------"
