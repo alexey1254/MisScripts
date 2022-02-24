dbed79908e49a3bcbe70502b9544beeee94ca796
Alejandro Marrero Cabrera <44745574+alexey1254@users.noreply.github.com>
2022-02-24 16:09:16 +0000

    Create Random.java

---
+++ JAVA/Random.java
@@ -0,0 +1,13 @@
+public class Random {
+    /**
+     * Devuelve un numero random comprendido entre min y max AMBOS inclusive
+     * @param min
+     * @param max
+     * @return Numero pseudo aleatorio
+     */
+    static long Random(long min, long max) {
+        long resto = min + 1 - max;
+        long numAleatorio = (long) (resto * Math.random() + max);
+        return numAleatorio;
+    }
+}
