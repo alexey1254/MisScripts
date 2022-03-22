9dfe81572063f38cc9c2fb8d32f0edb363e9c560
Alejandro Marrero Cabrera <44745574+alexey1254@users.noreply.github.com>
2022-03-22 16:00:29 +0000

    Se ha modificado la sintaxis

--- instalarApps.sh
+++ instalarApps.sh
@@ -3,27 +3,27 @@
 
 echo "--------------------- Se va a instalar Mysql-Server ---------------------"
 
-sudo apt update
-sudo apt install mysql-server -y
+`sudo apt update`
+`sudo apt install mysql-server -y`
 
 echo "--------------------- Se ha instalado mariaDB ---------------------"
 
-sudo add-apt-repository ppa:fkrull/deadsnakes
-sudo apt-get update
-sudo apt-get install python3.9 -y
+`sudo add-apt-repository ppa:fkrull/deadsnakes`
+`sudo apt-get update`
+`sudo apt-get install python3.9 -y`
 
 echo "--------------------- Se ha instalado python 3.9 ---------------------"
 
-sudo apt install openjdk-17-jdk -y
+`sudo apt install openjdk-17-jdk -y`
 
 echo "--------------------- Se ha instalado Java 17 ---------------------"
 
-sudo sudo snap install code --classic
+`sudo sudo snap install code --classic`
 
 echo "--------------------- Se ha instalado VS-Code ---------------------"
 
 echo " ---------------------Ahora se va a configurar MySql ---------------------"
 
-sudo mysql_secure_installation
+`sudo mysql_secure_installation`
 
 echo "--------------------- Se han completado todas las tareas ---------------------"
